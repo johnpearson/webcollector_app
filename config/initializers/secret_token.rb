926f3dbaffef7f06f3d5aedc9d22ebcd3ee271b6
jpearson <jpearson556@gmail.com>
2011-06-01 08:50:32 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebcollectorApp::Application.config.secret_token = 'c29d44b6415db7389794f32d6d5d82cdc8e410eb4d744022d203020121d6623011663aee2f8eaefd1acb22a0692a0656a40a260fa205a7bd8259ab932ad628ca'
