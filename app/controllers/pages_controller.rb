dff9bdce0a99e9a2fa41e7fb740427c33ef6fb52
jpearson <jpearson556@gmail.com>
2011-06-08 15:26:01 -0600

    Removed files

--- app/controllers/pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class PagesController < ApplicationController
-  # The Pages Controller handles all the static pages in this app
-  
-  # The home page is the root page for this app, it will basically just house the
-  # sign-in page for the whole application
-  def home
-   @title = 'Home'
-  end 
-end
