cca652b3413b7ced2295b202b7991562dd6ff33f
jpearson <jpearson556@gmail.com>
2011-06-10 12:02:07 -0600

    No paginatation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @folder = Folder.new
-    @folders = @user.folders
+    @folders = @user.folders.paginate(:page => params[:page], :per_page => 3)
     @title = current_user.email
   end
 end
